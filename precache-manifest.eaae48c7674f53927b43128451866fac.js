e386d41fa6e466e2fafcaf01c9473a8c23e2fcf4
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-06 14:49:47 -0400

    Updates

---
+++ precache-manifest.eaae48c7674f53927b43128451866fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0e1fd0046ed909708f79ae30f846f2",
+    "url": "%7Bdom2849%7D.github.io/%7BreactToDoList%7D/index.html"
+  },
+  {
+    "revision": "ea06868c786d27322dde",
+    "url": "%7Bdom2849%7D.github.io/%7BreactToDoList%7D/static/css/main.f8290274.chunk.css"
+  },
+  {
+    "revision": "6b9c067e191a76759b34",
+    "url": "%7Bdom2849%7D.github.io/%7BreactToDoList%7D/static/js/2.ba19787c.chunk.js"
+  },
+  {
+    "revision": "ea06868c786d27322dde",
+    "url": "%7Bdom2849%7D.github.io/%7BreactToDoList%7D/static/js/main.141edb39.chunk.js"
+  },
+  {
+    "revision": "f07b6842873ba77519ce",
+    "url": "%7Bdom2849%7D.github.io/%7BreactToDoList%7D/static/js/runtime~main.97e3adbb.js"
+  }
+]);
